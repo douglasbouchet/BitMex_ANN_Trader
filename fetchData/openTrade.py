36a7f95b426db6440586d0c4eb6bd53427469c90
douglas.bouchet@epfl.ch <douglas.bouchet@epfl.ch>
2021-02-02 17:11:00 +0100

    script to pick trade every 1/2/4 hours, and record it

---
+++ fetchData/openTrade.py
@@ -0,0 +1,59 @@
+import numpy as np
+import joblib
+import torch
+from getLastCandle import fetchData
+from LoadModel import loadModel, Model
+from saveRes import savePred
+from BitmexPrice import Get15mInd, Get1hInd, Get2hInd, Get4hInd
+
+
+def trade(timeframe):
+    switcher = {
+        "15_min": Get15mInd(),
+        "1_h": Get1hInd(),
+        "2_h": Get2hInd(),
+        "4_h": Get4hInd(),
+    }
+
+    cpT, rsiT, momT, maT = switcher.get(timeframe)
+
+    scalerCp = joblib.load('../saved_model/scaler/' +
+                           timeframe + '/scaler_cp.pkl')
+    scalerRsi = joblib.load('../saved_model/scaler/' +
+                            timeframe + '/scaler_rsi.pkl')
+    scalerMom = joblib.load('../saved_model/scaler/' +
+                            timeframe + '/scaler_momentum.pkl')
+
+    cp = np.array(cpT).reshape(1, np.array(cpT).shape[0])
+    rsi = np.array(rsiT).reshape(1, np.array(rsiT).shape[0])
+    momentum = np.array(momT).reshape(1, np.array(momT).shape[0])
+    scaledMa = np.array(maT).reshape(1, np.array(maT).shape[0])
+
+    scaledCp = scalerCp.transform(cp)
+    scaledRsi = scalerRsi.transform(rsi)
+    scaledMom = scalerMom.transform(momentum)
+
+    X = torch.Tensor(np.concatenate(
+        (scaledCp, scaledRsi, scaledMom, scaledMa), 1))
+
+    for delay in range(1, 17):
+        model = loadModel(timeframe, delay).eval()
+
+        out = model(X)
+        state: str = "0" if out[0][0] > out[0][1] else "1"
+        proba = format(out.detach()[0][int(state)].item(), '.3f')
+
+        savePred(delay, state, proba, timeframe)
+
+# TODO each timeframe: -> 15 min, 1 hour, 2 hour, 4 hour, run this given script:
+# each 15 min, script launch 15 min trade,
+# each hour, -> script launch 1 hour trade (15 min will also be launch)
+# Make a prediction for 1 to 20 delay of the current timeframe
+
+# record the prediction with the following:
+# 1: timestamp, 2: delay:, 3: long or short, 4: the timestamp of the objectif (see with delay)
+# Close ts represent the time at wich we need to compare the price of the btc
+
+
+# Then after some hours, we will fetch the new closes prices and open price, compare them
+# and compute the accuracy of the predictions.
--- fetchData/trade.py
+++ fetchData/trade.py
@@ -1,62 +1,38 @@
-import numpy as np
-import joblib
-import torch
-from getLastCandle import fetchData
-from LoadModel import loadModel, Model
-from saveRes import savePred
-from BitmexPrice import Get15mInd, Get1hInd, Get2hInd, Get4hInd
+import time
+import sched
+import schedule
+from openTrade import trade
 
 
-def trade(timeframe):
-    switcher = {
-        "15_min": Get15mInd(),
-        "1_h": Get1hInd(),
-        "2_h": Get2hInd(),
-        "4_h": Get4hInd(),
-    }
-    return switcher.get(timeframe)
+def p15m():
+    print("making a 15m prediction")
+    trade("15_min")
+    return
 
 
-timeframe = "4_h"
-delay = 3
-
-cpT, rsiT, momT, maT = trade(timeframe)
-
-scalerCp = joblib.load('../saved_model/scaler/' +
-                       timeframe + '/scaler_cp.pkl')
-scalerRsi = joblib.load('../saved_model/scaler/' +
-                        timeframe + '/scaler_rsi.pkl')
-scalerMom = joblib.load('../saved_model/scaler/' +
-                        timeframe + '/scaler_momentum.pkl')
-
-cp = np.array(cpT).reshape(1, np.array(cpT).shape[0])
-rsi = np.array(rsiT).reshape(1, np.array(rsiT).shape[0])
-momentum = np.array(momT).reshape(1, np.array(momT).shape[0])
-scaledMa = np.array(maT).reshape(1, np.array(maT).shape[0])
-
-scaledCp = scalerCp.transform(cp)
-scaledRsi = scalerRsi.transform(rsi)
-scaledMom = scalerMom.transform(momentum)
-
-X = torch.Tensor(np.concatenate((scaledCp, scaledRsi, scaledMom, scaledMa), 1))
-
-model = loadModel(timeframe, delay).eval()
-
-out = model(X)
-state: str = "0" if out[0][0] > out[0][1] else "1"
-proba = format(out.detach()[0][int(state)].item(), '.3f')
-
-savePred(delay, state, proba, timeframe)
-
-# TODO each timeframe: -> 15 min, 1 hour, 2 hour, 4 hour, run this given script:
-# each 15 min, script launch 15 min trade,
-# each hour, -> script launch 1 hour trade (15 min will also be launch)
-# Make a prediction for 1 to 20 delay of the current timeframe
-
-# record the prediction with the following:
-# 1: timestamp, 2: delay:, 3: long or short, 4: the timestamp of the objectif (see with delay)
-# Close ts represent the time at wich we need to compare the price of the btc
+def p1h():
+    print("making a 1 hour prediction")
+    trade("1_h")
+    return
 
 
-# Then after some hours, we will fetch the new closes prices and open price, compare them
-# and compute the accuracy of the predictions.
+def p2h():
+    print("making a 2 hour prediction")
+    trade("2_h")
+    return
+
+
+def p4h():
+    print("making a 4 hour prediction")
+    trade("4_h")
+    return
+
+
+# schedule.every(15).minute.at(":05").do(p15m)
+schedule.every(1).hour.at("00:03").do(p1h)
+schedule.every(2).hours.at("00:03").do(p2h)
+schedule.every(4).hours.at("00:05").do(p4h)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
